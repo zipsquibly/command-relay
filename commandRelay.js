c0bb588fb1f18f9c4800e48481e1710cb7b0d59f
Carlson, Brian <bcarlson@nextgen.com>
2013-10-16 18:14:23 -0500

    Added relay function

--- commandRelay.js
+++ commandRelay.js
@@ -1,11 +1,24 @@
 var io = require('socket.io').listen(8999);
+var uuid = require('uuid');
 
 var relay = io
   .of("/cmd-relay")
+  .authorization(function (handshakeData, callback) {
+    console.dir(handshakeData);
+    handshakeData.identifier = uuid();
+    callback(null, true);
+  })
   .on('connection', function (socket) {
-        console.log("connected");
+        socket.emit('identifier', socket.handshake.identifier);
         socket.on('echo', function (msg) {
             socket.emit('echo', msg);
         });
-  });
+        socket.on('link', function (id) {
+            socket.join(id);
+            socket.emit('linked');
+            socket.on('relay', function(data) {
+                socket.broadcast.to(id).emit('relay', data);
+            });
+        });
+    });
 
--- tests/test-relay.js
+++ tests/test-relay.js
@@ -3,20 +3,33 @@ var chai = require("chai"),
   assert = chai.assert,
   expect = chai.expect,
   util = require("util"),
+  child_process = require("child_process"),
   io = require("socket.io-client");
 
 describe('commandRelay', function() {
 
   var socket;
+  var trayClient;
+  var id = null;
   var HOST = "http://127.0.0.1:8999";
 
+  before(function(done) {
+      trayClient = child_process.fork('testClient.js');
+      trayClient.send('Froosh');
+      trayClient.on('message', function (m) {
+          console.log('parent: ' + m);
+      });
+      done();
+  });
   before(function(done) {
       require("../commandRelay.js");
       done();
   });
   it('should connect', function(done) {
     socket = io.connect(HOST + '/cmd-relay');
-    socket.on('connect', function () {
+    socket.on('identifier', function (ident) {
+        id = ident;
+        socket.emit('link', id);
         done();
     });
   });
@@ -26,5 +39,32 @@ describe('commandRelay', function() {
           done();
       });
       socket.emit('echo', 'hello');
+  });
+  it('should have an ID', function() {
+      expect(id).to.not.be.null;
+  });
+  it('should create second client', function(done) {
+      trayClient.send('connect');
+      trayClient.once('message', function(m) {
+          expect(m).to.be.equal('CONNECTED');
+          done();
+      });
+  });
+  it('should link both clients', function(done) {
+      trayClient.send({'id':id});
+      trayClient.once('message', function(m) {
+          expect(m).to.be.equal('LINKED');
+          done();
+      });
+  });
+  it('should relay a message', function(done) {
+    trayClient.send({'relay':'whassup'});
+    trayClient.once('message', function(m) {
+        expect(m).to.be.equal('RELAYED');
+    });
+    socket.once('relay', function (m) {
+        expect(m).to.be.equal('whassup');
+        done()
+    });
   });
 });
